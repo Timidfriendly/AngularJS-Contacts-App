3adf4eabb29ac65f432630819f2a0c753018bbab
Timidfriendly <timid@point-online.org>
2015-04-13 21:08:58 +0200

    1.6. Creating Our Resource

--- server.js
+++ server.js
@@ -4,11 +4,13 @@
  * Created by davidknowles on 04/04/15.
  */
 var express = require('express'),
+    api     = require('./api'),
     app     = express();
 
 
 app
     .use(express.static('./public'))
+    .use('/api', api)
     .get('*', function (req, res) {
         res.sendfile('public/main.html');
     })
